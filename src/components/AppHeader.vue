2d5d96f9951ec9ba2dec443bfa542cb29f480c91
Naoufal Ayache <naoufalayache@gmail.com>
2026-08-09 14:40:21 +0200

    Ajouts des titres en anglais

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,21 +18,54 @@ const closeMenu = () => {
   <header class="header">
     <RouterLink to="/" class="logo" @click="closeMenu"> NAOUFAL <span>AYACHE</span> </RouterLink>
     <nav class="navigation">
-      <RouterLink to="/">ACCUEIL</RouterLink> <RouterLink to="/AboutMe">A PROPOS DE MOI</RouterLink>
-      <RouterLink to="/projects">PROJETS</RouterLink>
-      <RouterLink to="/experience">EXPÉRIENCE</RouterLink>
-      <RouterLink to="/contacts">CONTACT</RouterLink> <LangSwitcher />
+      <RouterLink to="/">
+        {{ $t('navigation.home') }}
+      </RouterLink>
+
+      <RouterLink to="/AboutMe">
+        {{ $t('navigation.about') }}
+      </RouterLink>
+
+      <RouterLink to="/projects">
+        {{ $t('navigation.projects') }}
+      </RouterLink>
+
+      <RouterLink to="/experience">
+        {{ $t('navigation.experience') }}
+      </RouterLink>
+
+      <RouterLink to="/contacts">
+        {{ $t('navigation.contact') }}
+      </RouterLink>
+
+      <LangSwitcher />
     </nav>
+
     <button class="burger" type="button" aria-label="Ouvrir le menu" @click="toggleMenu">
       <i :class="menuOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'"></i>
     </button>
+
     <Transition name="mobile-menu">
       <nav v-if="menuOpen" class="mobile-menu">
-        <RouterLink to="/" @click="closeMenu">ACCUEIL</RouterLink>
-        <RouterLink to="/AboutMe" @click="closeMenu">A PROPOS DE MOI</RouterLink>
-        <RouterLink to="/projects" @click="closeMenu">PROJETS</RouterLink>
-        <RouterLink to="/experience" @click="closeMenu">EXPÉRIENCE</RouterLink>
-        <RouterLink to="/contacts" @click="closeMenu">CONTACT</RouterLink>
+        <RouterLink to="/" @click="closeMenu">
+          {{ $t('navigation.home') }}
+        </RouterLink>
+
+        <RouterLink to="/AboutMe" @click="closeMenu">
+          {{ $t('navigation.about') }}
+        </RouterLink>
+
+        <RouterLink to="/projects" @click="closeMenu">
+          {{ $t('navigation.projects') }}
+        </RouterLink>
+
+        <RouterLink to="/experience" @click="closeMenu">
+          {{ $t('navigation.experience') }}
+        </RouterLink>
+
+        <RouterLink to="/contacts" @click="closeMenu">
+          {{ $t('navigation.contact') }}
+        </RouterLink>
 
         <div class="mobile-language">
           <LangSwitcher />
